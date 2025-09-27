d2f6158c389dbe559bd7b9ab0dec5857d407c44e
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-09-27 18:32:25 +0200

    Uloha2

--- inf uloha prva.py
+++ inf uloha prva.py
@@ -1,10 +1,6 @@
-cislo1 = int(input("Zadaj prve cislo: "))
-cislo2 = int(input("Zadaj druhe cislo: "))
-sucin = cislo1 * cislo2
 
-if sucin > 100:
-    print("Sucin presiahol 100")
-elif sucin == 100:
-    print("Sucin je presne 100")
+cislo = float(input("Zadaj číslo: "))
+if cislo == 0:
+	print("Prevrátená hodnota neexistuje (delenie nulou).")
 else:
-    print("Sucin nepresiahol 100")
+	print("Prevrátená hodnota je:", 1/cislo)
