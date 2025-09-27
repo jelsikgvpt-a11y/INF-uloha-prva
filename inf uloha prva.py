0446cc42a440aa81c7e1246f3f32ee9a59cfc211
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-09-27 18:37:56 +0200

    Uloha 2 opravená

--- inf uloha prva.py
+++ inf uloha prva.py
@@ -1,9 +1,6 @@
-a = float(input("Zadaj dolnú hranicu intervalu a: "))
-b = float(input("Zadaj hornú hranicu intervalu b: "))
-x = float(input("Zadaj číslo x: "))
 
-if a < x < b:
-	print(f"Číslo {x} patrí do intervalu <{a}, {b}>.")
+cislo = float(input("Zadaj číslo: "))
+if cislo == 0:
+	print("Prevrátená hodnota neexistuje (delenie nulou).")
 else:
-	print(f"Číslo {x} nepatrí do intervalu <{a}, {b}>.")
-
+	print("Prevrátená hodnota je:", 1/cislo)
