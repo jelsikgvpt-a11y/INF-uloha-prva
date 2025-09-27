95a62cccef6fb01c74890973621298d32f4db557
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-09-27 18:51:11 +0200

    Uloha7

--- inf uloha prva.py
+++ inf uloha prva.py
@@ -1,9 +1,9 @@
-x = int(input("Zadaj číslo: "))
-if x % 4 == 0 and x % 7 == 0:
-	print(f"Číslo {x} je deliteľné aj štyrmi, aj siedmimi.")
-elif x % 4 == 0:
-	print(f"Číslo {x} je deliteľné štyrmi.")
-elif x % 7 == 0:
-	print(f"Číslo {x} je deliteľné siedmimi.")
+
+a = float(input("Zadaj prvé číslo: "))
+b = float(input("Zadaj druhé číslo: "))
+if a > b:
+	print(f"Väčšie číslo je: {a}")
+elif b > a:
+	print(f"Väčšie číslo je: {b}")
 else:
-	print(f"Číslo {x} nie je deliteľné ani štyrmi, ani siedmimi.")
+	print("Sú rovnaké")
