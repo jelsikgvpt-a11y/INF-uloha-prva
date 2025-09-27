3cf943867136d26ef5c140ae1c3c8aaa0b827dfb
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-09-27 18:49:17 +0200

    Uloha6

--- inf uloha prva.py
+++ inf uloha prva.py
@@ -1,6 +1,9 @@
-
 x = int(input("Zadaj číslo: "))
-if x % 3 == 0:
-	print(f"Číslo {x} je deliteľné troma.")
+if x % 4 == 0 and x % 7 == 0:
+	print(f"Číslo {x} je deliteľné aj štyrmi, aj siedmimi.")
+elif x % 4 == 0:
+	print(f"Číslo {x} je deliteľné štyrmi.")
+elif x % 7 == 0:
+	print(f"Číslo {x} je deliteľné siedmimi.")
 else:
-	print(f"Číslo {x} nie je deliteľné troma.")
+	print(f"Číslo {x} nie je deliteľné ani štyrmi, ani siedmimi.")
